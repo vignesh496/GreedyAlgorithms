d1e0f204a685b0dbfc0f351f4b167a458555c7ac
Vignesh G <115526040+vignesh496@users.noreply.github.com>
2024-03-12 22:14:07 +0530

    ActivitySelection.java

--- ActivitySelection.java
+++ ActivitySelection.java
@@ -1,46 +1,33 @@
 import java.io.*;
 import java.util.*;
 
-
 class Activity {
-	int start, finish;
-	Activity(int start, int finish)	{
-		this.start = start;
-		this.finish = finish;
-	}
+    int start, end;
+    Activity(int start, int end)    {
+        this.start = start;
+        this.end = end;
+    }
 }
 
-class Compare {
-	Activity[] a;
-	Compare(Activity[] a)	{
-		this.a = a;
-	}	
-	public void select()	{
-		Arrays.sort(a, Comparator.comparingInt(activity -> activity.finish));
-		int i = 0;
-		System.out.print("(" + a[i].start + ", " + a[i].finish + ") ");
-		for (int j = 1; j < a.length; j++)	{
-			if (a[j].start >= a[i].finish)	{
-				System.out.print("(" + a[j].start + ", " + a[j].finish + ") ");
-				i = j;
-			}
-		}
-	}
-}
-
-class ActivitySelection {
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter total activities : ");
-		int n = input.nextInt();
-		Activity[] a = new Activity[n];
-		System.out.println("Enter in (start, finish) order");
-		for (int i = 0; i < n; i++)	{
-			int s = input.nextInt();
-			int f = input.nextInt();
-			a[i] = new Activity(s, f);
-		}
-		Compare c = new Compare(a);
-		c.select();
-	}
+class ActivitySelection     {
+    static void select(int[] s, int[] f, int n)    {
+        Activity[] a = new Activity[n];
+        for (int i = 0; i < n; i++) 
+            a[i] = new Activity(s[i], f[i]);
+        Arrays.sort(a, Comparator.comparingInt(ac -> ac.end));
+        int i = 0;
+        System.out.println("Selected Activities: ");
+		System.out.println("(" + a[i].start + " " + a[i].end+ ")");
+        for (int j = 1; j < n; j++) {
+            if (a[j].start > a[i].end)   {
+                System.out.println("(" + a[j].start + " " + a[j].end+ ")");
+                i = j;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] s = {1,4,7,10,14,18};
+        int[] f = {3,6,9,12,15,20};
+        select(s, f, s.length);
+    }
 }
