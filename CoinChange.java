6c482cd72b4ee29032248248f4591afe9388b0d9
Vignesh G <115526040+vignesh496@users.noreply.github.com>
2024-05-23 10:13:00 +0530

    Update CoinChange.java

--- CoinChange.java
+++ CoinChange.java
@@ -1,30 +1,25 @@
 import java.util.*;
 
-public class CoinChange {
-
-    public static void coinChange(int[] coins, int[] counts, int amount) {
+class Main {
+    public static void main(String[] args) {
+        int[] coin = {1, 2, 5, 10};
+        int[] count = {10, 5, 2, 1};
+        coinChange(coin, count, 20);
+    }
+    public static void coinChange(int[] coin, int[] count, int amt)    {
         int tot = 0;
-        for (int i = coins.length - 1; i >= 0; i--) {
-            int count = Math.min(counts[i], amount / coins[i]);
-            if (count > 0) {
-                System.out.println("Rs." + coins[i] + "\t: "+ count + " coins");
-                amount -= (count * coins[i]);
-                tot += count;
+        for (int i = coin.length-1; i>=0; i--)  {
+            int c = Math.min(count[i], amt / coin[i]);
+            if (c > 0)  {
+                System.out.println("Rs." + coin[i] + " - " + count[i]);
+                amt -= (c * coin[i]);
+                tot += c;
             }
         }
-        
-        if (amount != 0) {
+        if (amt != 0)   {
             System.out.println("Cannot make coins");
             return;
         }
-        System.out.println("Total coins : " + tot); 
-    }
-
-    public static void main(String[] args) {
-        int[] coins = {1, 5, 10, 25};
-        int[] counts = {5, 3, 2, 1};   // Assuming limited number of coins
-        int amount = 56;
-        System.out.println("Coins used:");
-        coinChange(coins, counts, amount);
+        System.out.println("Total coins : " + tot);
     }
 }
